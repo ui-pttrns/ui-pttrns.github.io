9ea5dc39742fc602388d660ff16613430b300435
visualapps <rakesh.singh@mayadata.io>
2019-03-16 15:38:18 +0530

    Added magic grid

--- app/components/patterns-container/component.js
+++ app/components/patterns-container/component.js
@@ -1,12 +1,35 @@
 import Ember from 'ember';
 import Component from '@ember/component';
+import MagicGrid from 'magic-grid';
 
 export default Component.extend({
   modalService: Ember.inject.service('modal'),
 
+  loading: true,
+  magicGrid: null,
+
+  didInsertElement() {
+    this._super(...arguments);
+
+    let magicGrid = new MagicGrid({
+      container: ".grid", // Required. Can be a class, id, or an HTMLElement
+      animate: true,
+      gutter: 18,
+      static: true,
+      items: this.get('items').length,
+      useMin: true,
+      useTransform: false
+  });
+  this.set('magicGrid',magicGrid);
+  magicGrid.listen();
+  },
   actions: {
     openModal(itemurl){
-            this.get('modalService').toggleModal('modal-full-screen', itemurl);
-        },
+      this.get('modalService').toggleModal('modal-full-screen', itemurl);
+    },
+    placeholder() {
+      this.set('loading', false);
+      this.get('magicGrid').listen();
+    },
   }
 });
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -20,6 +20,9 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
+   app.import('node_modules/magic-grid/dist/magic-grid.min.js', {
+     using: [{ transformation: 'amd', as: 'magic-grid' }]
+    });
 
   return app.toTree();
 };
